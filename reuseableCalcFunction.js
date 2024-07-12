03510d74fc4f0400853a807faa5816e08d356dec
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-12 20:42:39 +0600

    file connected

--- reuseableCalcFunction.js
+++ reuseableCalcFunction.js
@@ -1,1 +1,20 @@
 
+function getElementById(idName){
+  const inputValueId = document.getElementById(idName)
+  const inputValue1 = inputValueId.value
+  const parse = parseFloat(inputValue1)
+  return parse 
+}
+
+
+function calculateTriangle(){
+    const b = getElementById('triangleB')
+    const h = getElementById('triangleH')
+    const area = .5 * b * h
+    getAreaById(area)
+}
+
+function getAreaById(areaId, area){
+    const areaId = document.getElementById(areaId)
+    areaId.innerText = area; 
+}
