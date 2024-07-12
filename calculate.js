4494a215622e357a6aedb8a615ccc90b68149383
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-12 19:27:38 +0600

    parallelogram added

--- calculate.js
+++ calculate.js
@@ -8,5 +8,77 @@ document.getElementById('triangleButton').addEventListener('click', function(){
     
     placeHolderB.value = ''
     placeHolderH.value = ''
-    
+
+    const getArea = document.getElementById('areaCalc')
+    getArea.innerText = multiplication
+})
+
+document.getElementById('rectangleButton').addEventListener('click', function(){
+    const placeHolderW = document.getElementById('rectangleW')
+    const w = parseFloat(rectangleW.value)
+    const placeHolderL = document.getElementById('rectangleL')
+    const l = parseFloat(rectangleL.value)
+    const rectangleCalc = w * l
+
+    placeHolderW.value = ''
+    placeHolderL.value = ''
+
+    const getArea = document.getElementById('areaCalc')
+    getArea.innerText = rectangleCalc
+})
+
+document.getElementById('parallelogramButton').addEventListener('click', function(){
+    const placeHolderB = document.getElementById('parallelogramB')
+    const b = parseFloat(parallelogramB.value)
+    const placeHolderH = document.getElementById('parallelogramH')
+    const h = parseFloat(parallelogramH.value)
+    const parallelogramCalc = b * h
+
+    placeHolderB.value = ''
+    placeHolderH.value = ''
+
+    const getArea = document.getElementById('areaCalc')
+    getArea.innerText = parallelogramCalc
+})
+
+document.getElementById('rectangleButton').addEventListener('click', function(){
+    const placeHolderW = document.getElementById('rectangleW')
+    const w = parseFloat(rectangleW.value)
+    const placeHolderL = document.getElementById('rectangleL')
+    const l = parseFloat(rectangleL.value)
+    const rectangleCalc = w * l
+
+    placeHolderW.value = ''
+    placeHolderL.value = ''
+
+    const getArea = document.getElementById('areaCalc')
+    getArea.innerText = rectangleCalc
+})
+
+document.getElementById('rectangleButton').addEventListener('click', function(){
+    const placeHolderW = document.getElementById('rectangleW')
+    const w = parseFloat(rectangleW.value)
+    const placeHolderL = document.getElementById('rectangleL')
+    const l = parseFloat(rectangleL.value)
+    const rectangleCalc = w * l
+
+    placeHolderW.value = ''
+    placeHolderL.value = ''
+
+    const getArea = document.getElementById('areaCalc')
+    getArea.innerText = rectangleCalc
+})
+
+document.getElementById('rectangleButton').addEventListener('click', function(){
+    const placeHolderW = document.getElementById('rectangleW')
+    const w = parseFloat(rectangleW.value)
+    const placeHolderL = document.getElementById('rectangleL')
+    const l = parseFloat(rectangleL.value)
+    const rectangleCalc = w * l
+
+    placeHolderW.value = ''
+    placeHolderL.value = ''
+
+    const getArea = document.getElementById('areaCalc')
+    getArea.innerText = rectangleCalc
 })
