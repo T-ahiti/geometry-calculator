8b9110623b00e735d70cd198f9f1bebcb3a1c94d
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-12 18:55:06 +0600

    multiplication added

--- calculate.js
+++ calculate.js
@@ -1,4 +1,12 @@
 
 document.getElementById('triangleButton').addEventListener('click', function(){
+    const placeHolderB = document.getElementById('triangleB')
+    const b = parseFloat(triangleB.value)
+    const placeHolderH = document.getElementById('triangleH')
+    const h = parseFloat(triangleH.value)
+    const multiplication = .5 * b * h 
+    
+    placeHolderB.value = ''
+    placeHolderH.value = ''
     
 })
